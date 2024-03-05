b4cc8f8c4991a5b84f9ea8c83b9e4437c3ce6918
Phuc Mai <mariamaihong1309@gmail.com>
2024-03-05 13:56:17 -0500

    orderDetails page

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -2,7 +2,7 @@ import { DataTable } from "@/components/custom ui/DataTable"
 import { columns } from "@/components/orderItems/OrderItemsColums"
 
 const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
-  const res = await fetch(`http://localhost:3000/api/orders/${params.orderId}`)
+  const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
   const { orderDetails, customer } = await res.json()
 
   const { street, city, state, postalCode, country } = orderDetails.shippingAddress
