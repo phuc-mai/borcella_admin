cb039965a7f69c95838ed66d545b63f7bf218bd3
Phuc Mai <mariamaihong1309@gmail.com>
2024-03-05 12:30:18 -0500

    key props for Image Upload

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -23,7 +23,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     <div>
       <div className="mb-4 flex flex-wrap items-center gap-4">
         {value.map((url) => (
-          <div className="relative w-[200px] h-[200px]">
+          <div key={url} className="relative w-[200px] h-[200px]">
             <div className="absolute top-0 right-0 z-10">
               <Button type="button" onClick={() => onRemove(url)} size="sm" className="bg-red-1 text-white">
                 <Trash className="h-4 w-4" />
